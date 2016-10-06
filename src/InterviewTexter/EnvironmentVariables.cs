d979c32c29cc7e86e99686242b7549e3bcfa64bf
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-10-05 23:34:48 -0700

    functionality to send text on a periodic basis

---
+++ src/InterviewTexter/EnvironmentVariables.cs
@@ -0,0 +1,10 @@
+﻿namespace InterviewTexter
+{
+    public static class EnvironmentVariables
+    {
+        public static string AccountSid = "ACc44c20aa77f64be71fbb93f0585571e8";
+        public static string AuthToken = "cefafee40e6197e7ea08f2e2561ba38f";
+        public static string TwilioPhone = "+19713024115";
+        public static string OwnPhone = "+15039567560";
+    }
+}
--- src/InterviewTexter/Program.cs
+++ src/InterviewTexter/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -12,7 +14,7 @@ namespace InterviewTexter
         {
             while (true)
             {
-                Thread.Sleep(10000);
+                Thread.Sleep(1 * 60 * 1000);
                 Console.WriteLine(DateTime.Now);
                 TextMeAQuestion();
             }
@@ -21,6 +23,21 @@ namespace InterviewTexter
         public static void TextMeAQuestion()
         {
             Console.WriteLine("cool");
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            //2
+            var request = new RestRequest("Accounts/" + EnvironmentVariables.AccountSid + "/Messages", Method.POST);
+            //3
+            request.AddParameter("To", EnvironmentVariables.OwnPhone);
+            request.AddParameter("From", EnvironmentVariables.TwilioPhone);
+            request.AddParameter("Body", "Hello world!");
+            //4
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSid, EnvironmentVariables.AuthToken);
+            //5
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+            //Console.ReadLine();
         }
     }  
 }
