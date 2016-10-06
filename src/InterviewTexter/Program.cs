cf32ed725d684c7e019374e50af982229b3472a6
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-10-05 23:06:44 -0700

    initial commit, set up timer loop

---
+++ src/InterviewTexter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InterviewTexter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine(DateTime.Now);
+                TextMeAQuestion();
+            }
+        }
+
+        public static void TextMeAQuestion()
+        {
+            Console.WriteLine("cool");
+        }
+    }  
+}
